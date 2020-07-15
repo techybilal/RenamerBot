a99912453ba6ace5107edd848b1f157b1c06244a
techybilal <41385844+techybilal@users.noreply.github.com>
2020-07-16 05:19:25 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,30 +1,30 @@
 class Translation(object):
-    START_TEXT = """ Simple Telegram Renaming BOT Edited By This <a href="https://tx.me/AID_3N">MAN</a>
-              🎈 @DX_BOTZ 🎈
+    START_TEXT = """ Hey Welcome to Any File Renamer Bot
+              🎈 @URobots 🎈
 <b>Please send me any Telegram file and reply /rename New Name.extn</b>
 SEE /help For My Features"""
 
     RENAME_403_ERR = "There Is Some Error With The File"
     ABS_TEXT = "What Are You Trying To Do,Mate?"
-    UPGRADE_TEXT = "CONTACT @AID_3N"
-    DOWNLOAD_START = "Give Me Some Time Niggah(〒﹏〒)"
-    UPLOAD_START = "Yay, Completed Downloading(;ŏ﹏ŏ)"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? use @PUBLIC_LEECH_2"
+    UPGRADE_TEXT = "CONTACT @URobot"
+    DOWNLOAD_START = "📥 𝑫𝒐𝒘𝒏𝒍𝒐𝒂𝒅𝒊𝒏𝒈 𝑷𝒍𝒆𝒂𝒔𝒆 𝑾𝒂𝒊𝒕 !!"
+    UPLOAD_START = "📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 𝙋𝙡𝙚𝙖𝙨𝙚 𝙒𝙖𝙞𝙩 !!"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It? Visit @URobots"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me,For Downloading Files Join @PUBLIC_LEECH_2**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Use @PUBLIC_LEECH_2 To Download From Links"
-    NOT_AUTH_USER_TEXT = "CONTACT @AID_3N"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @AID_3N"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds Visit @URobots For More Details"
+    NOT_AUTH_USER_TEXT = "CONTACT @LulSecX"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @LulSec"
     SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail Saved ✅ This Is Permanent Thumbnail ᕙ(  • ‿ •  )ᕗ "
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully😎"
-    CUSTOM_CAPTION_UL_FILE = """@PUBLIC_LEECH_2"""
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅𝗖𝘂𝘀𝘁𝗼𝗺 𝘁𝗵𝘂𝗺𝗯𝗻𝗮𝗶𝗹 𝗰𝗹𝗲𝗮𝗿𝗲𝗱 𝘀𝘂𝗰𝗰𝗲𝘀𝗳𝘂𝗹𝗹𝘆."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ 𝗠𝗲𝗱𝗶𝗮 𝗰𝗹𝗲𝗮𝗿𝗲𝗱 𝘀𝘂𝗰𝗰𝗲𝘀𝗳𝘂𝗹𝗹𝘆."
+    SAVED_RECVD_DOC_FILE = "𝗙𝗶𝗹𝗲 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗱 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆"
+    CUSTOM_CAPTION_UL_FILE = """Visit @URobots"""
     HELP_USER = """<b>How To Use Me</b>
     1. Send me any Telegram File(Some Files Gets Error)
     2. Reply to that message to /rename NEW NAME.EXTENSION
     FEATURES
     ➡Rename Telegram File With Custom Thumbnail.
     ➡Convert Streamable File(Video) To File.
-    ➡ FOR Downloading Files At Highspeed Join @PUBLIC_LEECH_2"""
+    ➡ Visit @URobots For More Details"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply The New Name With /rename Filename.extn"
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
